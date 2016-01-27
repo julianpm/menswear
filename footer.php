1c01aaa8f3727831b3487f3b12d209eb82561d11
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-01-27 14:21:56 -0500

    styling nav

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer>
-  <div class="container">
     <p>&copy;
     	<?php echo date('Y'); ?>
 		<?php echo bloginfo('name'); ?>
+		|
+		All Rights Reserved 
     </p>
-  </div>
 </footer>
 
 <script>
