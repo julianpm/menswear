7e77166b0c7f795d3e8a9e1f80200db8284128ca
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-01-28 17:27:16 -0500

    work on gallery

--- page.php
+++ page.php
@@ -8,14 +8,31 @@
           <?php the_content(); ?>
 
           <div class="stuff container">
-            
             <?php if ( get_field("home_title") ) : ?>
               <?php the_field("home_title") ; ?>
             <?php endif; ?>
-
           </div>
 
-        <?php endwhile; // end the loop?>
+          <div class="gallery container">
+          <?php 
+
+            $images = get_field('home_gallery');
+
+            if( $images ): ?>
+                <ul class="homeGallery slider flex">
+                    <?php foreach( $images as $image ): ?>
+                        <li>
+                            <a href="<?php echo $image['url']; ?>">
+                                 <img src="<?php echo $image['sizes']['square']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            </a>
+                            <p><?php echo $image['caption']; ?></p>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?> 
+          <!-- </div> END OF HOMEGALLERY -->         
+
+          <?php endwhile; // end the loop?>
 
   </div> <!-- /.main -->
 
---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+// Slider in Gallery
+
+$(document).ready(function(){
+	$('.slider').slick({
+  		dots: true,
+	  	infinite: true,
+	  	speed: 500,
+	  	fade: true,
+	  	cssEase: 'linear'
+	});
+});
